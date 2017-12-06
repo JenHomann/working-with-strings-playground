0ef5524ca3afbd691fbf89f8e62c50e746848b99
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 11:37:39 -0600

    initial commit

--- Contents.swift
+++ Contents.swift
@@ -2,6 +2,33 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str = "Hello"
 
 var newString = str + " Rob!"
+
+for character in newString.characters {
+    print(character)
+}
+
+// nsstring = Next Step String; has a number of functions available that String does not
+let newTypeString = NSString(string: newString)
+
+newTypeString.substring(to: 5)
+
+newTypeString.substring(from: 4)
+
+//challenge - use a combo of substring to and from to extract just the name Rob
+
+NSString(string: newTypeString.substring(from: 6)).substring(to: 3)
+
+newTypeString.substring(with: NSRange(location: 6, length: 3))
+
+if newTypeString.contains("Rob") {
+    print("newTypeString contains Rob!")
+}
+
+newTypeString.components(separatedBy: " ")
+
+newTypeString.uppercased
+newTypeString.lowercased
+newTypeString.capitalized
